6f825e4f1d687bc69d4e7dd0f8b4e89d1d0a525a
잠자는사자(Mac) <admin@sleepinglion.pe.kr>
2023-11-13 22:47:56 +0900

    수정

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :deploy_to, "/home/deploy/anti-kb"
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-set :pty, true
+#set :pty, true
 set :rbenv_type, :user
 set :rbenv_ruby, "3.2.2"
 
