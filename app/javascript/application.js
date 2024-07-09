dad296992da8a75a41820c3a78dbb41e1fe4ef75
잠자는사자(Mac) <admin@sleepinglion.pe.kr>
2024-07-09 16:53:32 +0900

    rails7 update

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+import "@hotwired/turbo-rails"
+import * as bootstrap from "bootstrap"
+import './add_jquery'
+import '@fancyapps/fancybox';
+import  'jquery-tagcanvas';
+import './common'
+import './index'
+
