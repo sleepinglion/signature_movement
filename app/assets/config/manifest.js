7d032093dc7d1e749848810e48c8df50273de47f
sleepinglion <admin@sleepinglion.pe.kr>
2024-12-25 13:30:14 +0900

    modified:   app/assets/config/manifest.js 	modified:   config/deploy.rb

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,7 @@
 //= link_tree ../images
 //= link_tree ../fonts
 //= link_directory ../stylesheets .css
-//= link_directory ../../javascript .js
+//= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link front/application.css
 //= link rails-ujs.js
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,46 +1,23 @@
 # config valid for current version and patch releases of Capistrano
-
 set :application, "anti-kb"
 set :repo_url, "git@github.com:sleepinglion/signature_movement.git"
+set :branch, 'master'
+set :deploy_to, '/home/deploy/anti-kb'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# set :format, :pretty
+# set :log_level, :debug
+#set :pty, true
+set :linked_files, %w{config/database.yml config/master.key .env}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/assets public/uploads}
 
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/anti-kb"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-set :pty, true
 set :rbenv_type, :user
 set :rbenv_ruby, "3.3.4"
 set :ssh_options, verify_host_key: :never
 set :default_env, {'NODE_OPTIONS'=>'--openssl-legacy-provider'}
 
-# Default value for :linked_files is []
-append :linked_files, "config/database.yml", 'config/master.key', ".env"
-
-# Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-
-# Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 5
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-set :keep_releases, 2
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
-#
 namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
