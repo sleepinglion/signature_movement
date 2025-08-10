7fb2e0e110da585531c9ef9ab5528cf2a58f6cc6
sleepinglion <admin@jedaeroweb.co.kr>
2025-08-10 15:18:01 +0900

    수정함:        config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,7 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/assets public/upl
 
 set :rbenv_type, :user
 set :rbenv_ruby, "3.3.4"
+set :rbenv_prefix, "$HOME/.rbenv/bin/rbenv exec"
 set :ssh_options, verify_host_key: :never
 set :default_env, {'NODE_OPTIONS'=>'--openssl-legacy-provider'}
 set :bundle_path, '/usr/local/bundle'
